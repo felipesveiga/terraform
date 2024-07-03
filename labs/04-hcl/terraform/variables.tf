2bbaaa76eb683f59438beafae4f273cf253bb939
felipesveiga <felipesveiga@gmail.com>
2024-07-03 08:42:03 -0300

    Corrigir bug no `terraform plan`

--- labs/04-hcl/terraform/variables.tf
+++ labs/04-hcl/terraform/variables.tf
@@ -32,14 +32,17 @@ variable "public_subnets" {
 variable "variables_sub_cidr" {
   description = "CIDR Block for Variables Subnet"
   type        = string
+default = "10.0.202.0/24" 
 }
 
 variable "variables_sub_az" {
   description = "Availability Zone for Variables Subnet"
   type        = string
+default="us-east-1"
 }
 
 variable "variables_sub_auto_ip" {
   description = "Set Automatic IP Assignment for Variables Subnet"
   type        = bool
+default=true
 }
