a1e3bd2b10d692562f890f061fbbe7e6a5ffa479
felipesveiga <felipesveiga@gmail.com>
2024-09-23 06:36:03 -0300

    Inserir variáveis AWS no Workspace

--- labs/10-backend/terraform/terraform.tf
+++ labs/10-backend/terraform/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
 
   backend "remote" {
-    hostname = "terraform.io"
+    hostname = "app.terraform.io"
     organization = "Veiga_LTDA"
 
     workspaces {
