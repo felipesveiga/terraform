17e3a7c385c8dfee746f1d991e2b9cd759cf8922
felipesveiga <felipesveiga@gmail.com>
2024-10-30 08:55:18 -0300

    Aula 64 (5:55)

--- labs/10-backend/terraform/main.tf
+++ labs/10-backend/terraform/main.tf
@@ -132,3 +132,29 @@ resource "aws_subnet" "list_subnet" {
   cidr_block        = each.value["ip"]
   availability_zone = each.value["az"]
 }
+
+locals {
+  ingress_rules = [{
+    port        = 443
+    description = "Port 443"
+    },
+    {
+      port        = 80
+      description = "Port 80"
+    }
+  ]
+}
+resource "aws_security_group" "main" {
+  name   = "core-sg"
+  vpc_id = aws_vpc.vpc.id
+  dynamic "ingress" {
+    for_each = local.ingress_rules
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
